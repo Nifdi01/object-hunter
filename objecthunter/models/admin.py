3757d19dbaccf85c13694592bc5b91d5c39aeb31
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2023-09-07 18:20:40 +0400

    OHGB-1: Added models to admin panel

--- objecthunter/models/admin.py
+++ objecthunter/models/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CategoryLabel, ObjectDetectionModel
 
-# Register your models here.
+class CategoryLabelAdmin(admin.ModelAdmin):
+    list_display = ('label_name', 'get_label_ids')
+
+    def get_label_ids(self, obj):
+        # This method returns a comma-separated string of label IDs associated with the CategoryLabel
+        return ', '.join([str(label.label_id) for label in obj.label_ids.all()])
+
+    # Set a user-friendly column header for get_label_ids method
+    get_label_ids.short_description = 'Label IDs'
+
+admin.site.register(CategoryLabel, CategoryLabelAdmin)
+
+admin.site.register(ObjectDetectionModel)
