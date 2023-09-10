451bdfb128907ce90447c19b6ab4a9793a2b3467
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2023-09-10 18:30:21 +0400

    [MAIN] database fix

--- objecthunter/models/management/commands/create_object_detection_models.py
+++ objecthunter/models/management/commands/create_object_detection_models.py
@@ -13,6 +13,7 @@ class Command(BaseCommand):
             'Sports and Recreation': [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
             'Food and Drinks': [47, 48, 49, 50, 51, 52, 53, 54, 55, 56],
             'Furniture': [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 76, 77, 78, 79, 80],
+            'People': [0, 1]
         }
 
         # Create models for each category label
